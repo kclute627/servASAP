fd48f419b0726f9885958c31ee0832ae0bef2fd9
Kyle C <kyclutter@gmail.com>
2021-02-02 08:31:07 -0600

    updates

--- src/Components/Jobs/AddJob.js
+++ src/Components/Jobs/AddJob.js
@@ -2,23 +2,16 @@ import React, { useState, useEffect } from "react";
 import AddJobForm from "./AddJobForm";
 
 const AddJob = () => {
-  
-  useEffect(()=> {
-    
-    window.scrollTo(0, 0)
-  }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="addjob">
       <div className="addjob-top">
         <h1>ADD A JOB</h1>
       </div>
 
-      <form className="form-group">
-        
-          <AddJobForm />
-       
-      
-      </form>
+      <AddJobForm />
     </div>
   );
 };
